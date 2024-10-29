d5eb5dacd92a5559077875a31fbb5df4b655abc8
yahong <yechan0815@naver.com>
2024-10-30 00:54:18 +0900

    relocation

--- bfd/elf32-amo.c
+++ bfd/elf32-amo.c
@@ -35,11 +35,11 @@ struct amo_relocation_map
 static const struct amo_relocation_map amo_reloc_map[] =
 {
 	{ BFD_RELOC_NONE, R_AMO_NONE },
-    { BFD_RELOC_32, R_AMO_32 },
 	{ BFD_RELOC_AMO_LITERAL, R_AMO_LITERAL },
 	{ BFD_RELOC_AMO_PCREL, R_AMO_PCREL },
 	{ BFD_RELOC_AMO_28, R_AMO_28 },
-	{ BFD_RELOC_AMO_32, R_AMO_32 }
+	{ BFD_RELOC_AMO_32, R_AMO_32 },
+    { BFD_RELOC_32, R_AMO_32 }
 };
 
 static bfd_reloc_status_type
